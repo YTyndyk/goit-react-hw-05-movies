e23d7528db35346b0c7c29d0e35aca829beba925
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-05-31 19:01:35 +0300

    commit

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Poster } from './MovieCard.styled';
 export const MovieCard = props => {
-  // const [score, setScore] = useState(0);
+  const [score, setScore] = useState(0);
   // console.log({ props });
   const { poster_path, title, release_date, overview, vote_average, genres } =
     props;
@@ -9,13 +9,13 @@ export const MovieCard = props => {
   const defaultImage =
     'https://holstovich.art/content/images/1/1000x1000l80br20/kartina-russkiy-voennyy-korabl-idi-nakhy-50804578290634.webp';
 
-  // const userScore = (vote_average * 10).toFixed();
-  // setScore(userScore);
+  const userScore = (vote_average * 10).toFixed();
+  setScore(userScore);
   return (
     <>
       <Poster src={poster_path ? poster : defaultImage} alt={title} />
       <h1>{title}</h1>
-      {/* <p>User score: {score}%</p> */}
+      <p>User score: {score}%</p>
       <p>({release_date.slice(0, 4)})</p>
       <h2>Overview</h2>
       <p>{overview}</p>
